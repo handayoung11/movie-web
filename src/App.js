5501159da2748ad3d1aee456b7afee1d4f8947b9
JinYeongKim <handayoung11@gmail.com>
2022-11-02 23:55:08 +0900

    간단한 todo app 작성 - input 입력하고 버튼 클릭 시 todos 배열에 항목 추가되도록 로직 작성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import Button from "./Button";
+import { useState } from "react";
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
+  const onChange = (e) => setToDo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (toDo === "") {
+      return;
+    }
+
+    console.log(toDo);
+    setToDos((cur) => [toDo, ...cur]);
+    setToDo("");
+  };
+  console.log(toDos);
+
   return (
     <div>
-      <h1>Movie Web</h1>
-      <Button text="Continue" />
+      <h1>To Dos({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={toDo} onChange={onChange} />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
