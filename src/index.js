0ca405581089360c6fe0216e920236fa34b141fa
JinYeongKim <handayoung11@gmail.com>
2023-01-16 11:15:14 +0900

    React Router 적용

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/movie",
+    element: <Detail />,
+  },
+]);
+
+const app = ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
+
+export default app;
